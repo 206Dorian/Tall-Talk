3df8b3394d6459b9c90b9db4cec0d18974b32bd9
Dorian Birch <206dorian@gmail.com>
2023-05-29 20:39:48 -0500

    settup up clinet side and sever side files a d did rerouting, seesm to be working now

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+function generateRandomQuestions() {
+    var questions = [
+        "What is your favorite Animal?",
+
+        "Where in all the world would you like to be transported to?",
+        
+        "If you could leave earth to travel the universe, but you could never return would you?",
+        
+        "Who hurt you?",
+        
+        "what type of weather is your favorite?"
+    ];
+    
+    var randomIndex = Math.floor(Math.random() * questions.length);
+    return questions[randomIndex];
+}
+var randomQuestion = generateRandomQuestions();
+console.log(randomQuestion);
